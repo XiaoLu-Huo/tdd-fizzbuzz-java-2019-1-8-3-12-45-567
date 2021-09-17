301a03606deb5f39d35afb7eff6e4e5f9b00e54c
huoxiaolu <1821100432@qq.com>
2021-09-17 21:48:34 +0800

    [huoxiaolu] refactor:提取getResult方法

--- src/main/java/com/thoughtworks/tdd/FizzBuzzWhizz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzWhizz.java
@@ -2,11 +2,14 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzzWhizz {
     public String fizzBuzz(Integer input) {
+        return getResult(input).equals("") ? input.toString() : getResult(input);
+    }
+
+    private String getResult(Integer input) {
         String fizz = fizzResult(input);
         String buzz = buzzResult(input);
         String whizz = whizzResult(input);
-        String result = fizz + buzz + whizz;
-        return result.equals("") ? input.toString() : result;
+        return fizz + buzz + whizz;
     }
 
     private String whizzResult(int input) {
